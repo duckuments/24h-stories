3ed88b86667aaac49efbb3645bb69ff4787729ab
duckuments <duckuments@gmail.com>
2025-10-06 13:25:15 +0330

    refactor: 💡 remove static stories data

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,12 @@ import AddButton from "./components/AddButton"
 import Stories from "./components/Stories"
 
 function App() {
-  const items = [
-    { src: "https://picsum.photos/100" },
-    { src: "https://picsum.photos/100" },
-    { src: "https://picsum.photos/100" },
-    { src: "https://picsum.photos/100" },
-    { src: "https://picsum.photos/100" },
-  ]
-
   return (
     <div className="min-w-fit py-10">
       <h1 className="font-bold capitalize text-4xl px-10 mb-9">Stories</h1>
       <div className='flex min-w-fit flex-row gap-6 px-10 py-4 bg-base-200'>
         <AddButton />
-        <Stories data={items} />
+        <Stories />
       </div>
     </div>
   )
